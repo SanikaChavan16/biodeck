705ebc4aac2bf2979274ce0e6eb8011b28db60dc
Sanika Chavan <sanika16chavan.com>
2025-09-06 13:04:26 +0530

    use REACT_APP_API_URL from env

--- client/src/api.js
+++ client/src/api.js
@@ -1,9 +1,11 @@
 // client/src/api.js
 import axios from "axios";
 
-const API = axios.create({
-  baseURL: "http://localhost:5000/api",
-  withCredentials: true,
+const API_BASE = process.env.REACT_APP_API_URL || "http://localhost:5000/api";
+
+const api = axios.create({
+  baseURL: API_BASE,
+  withCredentials: true, // keep if you use cookies; remove if JWT-only
 });
 
-export default API;
+export default api;
